073ae154f9bcc65bea50b2729326aea6bad5cd13
Anirutth2929 <anirutthsa@gmail.com>
2026-01-06 19:33:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import pymysql
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def home():
     conn = pymysql.connect(
         user="root",
         password="Akss2929!",
-        unix_socket="/cloudsql/project-70309335-c83d-4b66-8d2:us-central1:root"
+        unix_socket="/cloudsql/project-70309335-c83d-4b66-8d2:us-central1:my-mysql",
         database="testdb"
     )
 
