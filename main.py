a1f805e57c77e86c60b3db5e3513d64a3f6c7ada
Anirutth2929 <anirutthsa@gmail.com>
2026-01-06 15:53:25 +0530

    Initialize Flask app with MySQL connection

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import pymysql
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    conn = pymysql.connect(
+        user="root",
+        password="Akss2929!",
+        unix_socket="/cloudsql/project-70309335-c83d-4b66-8d2:us-central1:my-mysql",
+        database="testdb"
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+
+    html = "<h2>Users</h2><table border=1>"
+    for r in rows:
+        html += "<tr>" + "".join(f"<td>{c}</td>" for c in r) + "</tr>"
+    html += "</table>"
+
+    cur.close()
+    conn.close()
+    return html
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
