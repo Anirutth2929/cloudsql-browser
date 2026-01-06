600494abb0a0fe9ffb67f4b7b07d91c27f46c509
Anirutth2929 <anirutthsa@gmail.com>
2026-01-06 19:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def home():
     conn = pymysql.connect(
         user="root",
         password="Akss2929!",
-        unix_socket="/cloudsql/project-70309335-c83d-4b66-8d2:us-central1:my-mysql",
+        unix_socket="/cloudsql/project-70309335-c83d-4b66-8d2:us-central1:root"
         database="testdb"
     )
 
