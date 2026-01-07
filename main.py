0dec9352ef08078dc2f66716b03e4957116638d9
Anirutth2929 <anirutthsa@gmail.com>
2026-01-07 11:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-from flask import Flask
 import pymysql
 import os
 
-app = Flask(__name__)
+def main():
+    print("Job started")
 
-@app.route("/")
-def home():
     conn = pymysql.connect(
-        user="root",
-        password="Akss2929!",
-        unix_socket="/cloudsql/project-70309335-c83d-4b66-8d2:us-central1:root",
-        database="testdb"
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        unix_socket=f"/cloudsql/{os.environ['INSTANCE_CONNECTION_NAME']}",
+        database=os.environ["DB_NAME"]
     )
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users")
-    rows = cur.fetchall()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
 
-    html = "<h2>Users</h2><table border=1>"
-    for r in rows:
-        html += "<tr>" + "".join(f"<td>{c}</td>" for c in r) + "</tr>"
-    html += "</table>"
+    for row in rows:
+        print(row)
 
-    cur.close()
+    cursor.close()
     conn.close()
-    return html
+
+    print("Job finished successfully")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    main()
